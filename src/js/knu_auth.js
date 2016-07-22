33013d00145b136863dcf43edb2c8acead4e657a
OhChang Kwon <ohchang.kwon@navercorp.com>
2016-07-22 18:10:24 +0900

    knu 자동 로그인 모듈 완성

---
+++ src/js/knu_auth.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const request = require("request");
+
+exports = module.exports = KnuAuth;
+
+function KnuAuth(knuId, knuPassword, stuNumber) {
+  this.knuId = knuId;
+  this.knuPassword = knuPassword;
+  this.stuNumber = stuNumber;
+  this.cookieJar = request.jar();
+}
+
+KnuAuth.prototype.getDiv = function(divCallback) {
+  var that = this;
+  console.log("getDiv");
+
+  request({
+      method: "POST",
+      url: "http://my.knu.ac.kr/stpo/comm/support/loginPortal/getAvailableUserDivs.action",
+      headers: {
+        'content-type': 'multipart/form-data',
+        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+      },
+      formData: {
+        "user.usr_id": that.knuId
+      },
+      jar : that.cookieJar
+    }, divCallback);
+};
+
+KnuAuth.prototype.login = function(loginCallback) {
+  console.log("login");
+  var that = this;
+
+  new Promise((resolve, reject) => {
+    that.getDiv((error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+
+      console.log(`STATUS: ${response.statusCode}`);
+      console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+      console.log(`BODY: ${body.toString()}`);
+
+      resolve(JSON.parse(body.replace(/'/g,"\""))[0].key);
+    });
+  })
+  .then((div) => {
+    request({
+      method: "POST",
+      url: "http://my.knu.ac.kr/stpo/comm/support/loginPortal/login.action",
+      headers: {
+        'content-type': 'multipart/form-data',
+        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+      },
+      formData: {
+        // "redirUrl": "/stpo/stpo/main/main.action",
+        // "menuParam": "901",
+        "user.usr_id": that.knuId,
+        "user.passwd": that.knuPassword,
+        "user.user_div": div,
+        "user.stu_persnl_nbr": that.stuNumber
+      },
+      jar: that.cookieJar
+    }, loginCallback);
+  });
+};
+
+KnuAuth.prototype.getCookie = function() {
+  return this.cookieJar.getCookies("http://my.knu.ac.kr", {allPaths: true});
+};
--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 "use strict";
 const electron = require('electron');
-const {app} = electron;
-const {BrowserWindow} = electron;
+const {app, ipcMain, BrowserWindow} = electron;
+const KnuAuth = require("./js/knu_auth.js");
 
 let win;
+let winSession;
+let loginChild;
+let loginDataCache;
 
 function createWindow() {
   win = new BrowserWindow({width: 800, height: 600});
-  win.loadURL(`file://${__dirname}/html/index.html`);
+  loginChild = new BrowserWindow({width: 200, height: 200,
+    frame: false, parent: win, transparent: false});
+
+  // win.loadURL(`file://${__dirname}/html/index.html`);
+  win.loadURL("http://my.knu.ac.kr");
+  winSession = win.webContents.session;
+  // win.openDevTools(["detach"]);
+  loginChild.loadURL(`file://${__dirname}/html/auth.html`);
+  // loginChild.openDevTools(["detach"]);
 
   win.on('closed', () => {
     win = null;
@@ -25,4 +36,66 @@ app.on('activate', () => {
   if (win === null) {
     createWindow();
   }
+});
+
+ipcMain.on("startLogin", (event, data) => {
+  console.log("start Login");
+  console.log(data);
+
+  if (!data) {
+    data = loginDataCache;
+  } else {
+    loginDataCache = data;
+  }
+
+  if (!data || !data.id || !data.password || !data.stu_number) {
+    event.sender.send("postLogin", {
+      error: "아이디, 비밀번호, 학번을 모두 입력하세요.",
+      response: null
+    });
+
+    return;
+  }
+
+  var knuAuth = new KnuAuth(data.id, data.password, data.stu_number);
+  knuAuth.login((err, response, body) => {
+    if (err) {
+      console.log(err);
+      throw new Error(err);
+    }
+
+    console.log(`STATUS: ${response.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+    console.log(`BODY: ${body}`);
+    console.log(`COOKIE: ${knuAuth.getCookie()}`);
+
+    var setCallback = function(error) {
+      if (error) {
+        console.log(error);
+      }
+    };
+    var cookies = knuAuth.getCookie();
+    for (let i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var details = {
+        "url" : "http://my.knu.ac.kr",
+        "name": cookie.key,
+        "value": cookie.value,
+        "domain": (cookie.domain === "my.knu.ac.kr") ? null : cookie.domain,
+        "path": cookie.path,
+        "secure": cookie.secure,
+        "httpOnly": cookie.httpOnly,
+        "expirationDate": cookie.expires
+      };
+
+      winSession.cookies.set(details, setCallback);
+    }
+
+    win.loadURL("http://my.knu.ac.kr");
+  });
+
+  event.sender.send("postLogin", {
+    error: null,
+    response: "test"
+  });
 });
